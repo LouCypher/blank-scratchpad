bf4f07ccf8df6271b559572f57a983ad6294abe3
LouCypher <zoolcar9+dev@gmail.com>
2014-01-07 14:04:06 +0700

    Backward compatibility

--- src/scratchpad-blank.js
+++ src/scratchpad-blank.js
@@ -1,24 +1,39 @@
-/*
- *  This Source Code Form is subject to the terms of the Mozilla Public
+/*  This Source Code Form is subject to the terms of the Mozilla Public
  *  License, v. 2.0. If a copy of the MPL was not distributed with this
- *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- *  Contributor(s):
- *  - LouCypher (original code)
- */
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 (function() {
   function $(aId) document.getElementById(aId);
 
   function clearInitialText() {
-    let prettyKey = ShortcutUtils.prettifyShortcut;
-    let initialText = Scratchpad.strings.formatStringFromName(
-                      "scratchpadIntro1", [prettyKey($("sp-key-run")),
-                                           prettyKey($("sp-key-inspect")),
-                                           prettyKey($("sp-key-display"))], 3);
+    var initialText = null;
+    try {
+      initialText = Scratchpad.strings.GetStringFromName("scratchpadIntro");
+    }
+    catch (ex) {
+      var prettyKey = "DevtoolsHelpers" in window
+                        ? DevtoolsHelpers.prettyKey
+                        : "LayoutHelpers" in window
+                          ? LayoutHelpers.prettyKey
+                            : "ShortcutUtils" in window         // Firefox 27+
+                              ? ShortcutUtils.prettifyShortcut
+                              : SP_Pretty_Key;
 
-    if (Scratchpad.editor.config.value === initialText)
-      Scratchpad.editor.config.value = "";
+      initialText = Scratchpad.strings.formatStringFromName("scratchpadIntro1",
+                                                            [prettyKey($("sp-key-run")),
+                                                             prettyKey($("sp-key-inspect")),
+                                                             prettyKey($("sp-key-display"))],
+                                                            3);
+    }
+
+    if ("config" in Scratchpad.editor) {  // Firefox 27+
+      if (Scratchpad.editor.config.value === initialText)
+        Scratchpad.editor.config.value = "";
+    }
+    else {
+      if (Scratchpad.editor._config.initialText === initialText)
+        Scratchpad.editor._config.initialText = "";
+    }
   }
 
   addEventListener("load", clearInitialText);
